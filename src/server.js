c04d4e55a719e1d5b1d2d2b42a20c9d9b63ad1df
probeshnath <probeshn@gmail.com>
2023-12-18 21:04:09 +0600

    home basic get request add

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+app.get("/",(req,res) =>{
+    res.send("Hi home route")
+})
 
 app.listen(3001, () =>{
     console.log(` server is runing at http://localhost:3001`)
