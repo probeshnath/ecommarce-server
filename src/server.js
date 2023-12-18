8c5f04d39f7579592a7e9c5cb4604f07ea8b8870
probeshnath <probeshn@gmail.com>
2023-12-18 21:08:26 +0600

    products basic route added

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ app.get("/",(req,res) =>{
     res.send("Hi home route")
 })
 
+app.get("/products",(req,res) =>{
+    res.send("products are come back on the market")
+})
+
 app.listen(3001, () =>{
     console.log(` server is runing at http://localhost:3001`)
 } )
