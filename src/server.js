07374d03d0f0f14e13e61397b01721cdfe73753b
probeshnath <probeshn@gmail.com>
2023-12-19 20:22:24 +0600

    post request testing

--- src/server.js
+++ src/server.js
@@ -13,6 +13,10 @@ app.get("/products",(req,res) =>{
     res.status(200).json({message: "products are come back on the market"})
    
 })
+app.post("/products",(req,res) =>{
+    res.status(200).json({message: "products post request are come back on the market"})
+   
+})
 
 app.listen(3001, () =>{
     console.log(` server is runing at http://localhost:3001`)
