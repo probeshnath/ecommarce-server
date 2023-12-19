1cedb512badf60c87252755c37799fe9926dc81c
probeshnath <probeshn@gmail.com>
2023-12-19 20:06:01 +0600

    start and dev properties added

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req,res) =>{
-    res.status(200).json({message: "Hi home route"})
+    res.status(200).json({message: "Hi home route..... fine"})
 })
 
 app.get("/products",(req,res) =>{
