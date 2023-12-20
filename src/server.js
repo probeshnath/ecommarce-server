9ab0c1ebfd1e619a4c32c0f834a655a7163a0cf4
probeshnath <probeshn@gmail.com>
2023-12-20 13:58:05 +0600

    server error handliing

--- src/server.js
+++ src/server.js
@@ -41,6 +41,12 @@ app.post("/products",(req,res) =>{
 // client error handliing
 app.use((req,res,next) =>{
     res.status(404).json({message: "Route is not found"})
+    next()
+})
+// server error handliing
+app.use((err,req,res,next) =>{
+    console.error(err.stack)
+    res.status(500).json({message: "something broken in server"})
 })
 
 
