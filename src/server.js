b21bf29b4869530d17b990d3e5fec2caa7ea5f69
probeshnath <probeshn@gmail.com>
2023-12-20 13:49:35 +0600

    bodyParser use

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const morgan = require('morgan')
+const bodyParser = require('body-parser');
 const app = express();
 
 // middleware
 app.use(morgan("dev"))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 const isLoggedIn = (req,res,next) =>{
 // console.log("isLoggedIn Middleware")
