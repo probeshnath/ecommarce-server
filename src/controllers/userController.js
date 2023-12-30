c18eb2a6c0931eebd3a289abbc63ecbd077aed17
probeshnath <probeshn@gmail.com>
2023-12-30 11:50:10 +0600

    fetch users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,14 @@
 const createError = require('http-errors')
+const User = require('../models/userModel')
 
 // get user 
-const getUsers =  (req, res,next) => {
+const getUsers = async  (req, res,next) => {
     
     try {
+        const users = await User.find()
         res.status(200).send({ 
             message: "Users were return",
+            users
          })
     } catch (error) {
         next(error)
