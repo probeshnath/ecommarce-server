fa3adde741b0a288115dbc26e2db05ecd519c9e3
probeshnath <probeshn@gmail.com>
2024-01-10 23:44:12 +0600

    jwt proccess modify and error handle

--- src/helper/jsonwebtoken.js
+++ src/helper/jsonwebtoken.js
@@ -1,8 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 const createJsonWebToken = (payload, secretKey, expiresIn) =>{
+
+    // error handle
+    if(typeof payload !== 'object' || !payload ){
+        throw new Error('Payload must be a non-empty object')
+    }
+    if(typeof secretKey !== 'string' || secretKey === "" ){
+        throw new Error('secretKey must be a non-empty string')
+    }
+
+   try {
+
     const token = jwt.sign(payload, secretKey,{expiresIn})
     return token
+
+   } catch (error) {
+    console.error('Failed to sign the jwt', error)
+    throw error;
+   }
 }
 
 module.exports = {createJsonWebToken}
